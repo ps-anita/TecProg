3b5d9f08556530fd77095a76f1901d162b8c67f3
Ro Widmer <rociowidmer@gmail.com>
2025-04-25 02:44:00 -0300

    cambie lugar clases, (+ metodos): asiento,unidad,reserva,servicio

--- TP.py
+++ TP.py
@@ -1,13 +1,28 @@
 from datetime import datetime, date
 from abc import ABC, abstractmethod
 
-class ArgenTUR:
-    def __init__(self):
-        self.sistemaActivo = True
+
+class Asiento:
+    def __init__(self, numero: int, ocupado: bool = False):
+        self.numero = numero
+        self.ocupado = ocupado
+    def obtenerNumero(self):
+        return self.numero
+    def verificarOcupacion(self)->bool:
+        return self.ocupado
+    def cambiarEstado(self):
+        self.ocupado = not self.ocupado
 
 class Unidad:
     def __init__(self, patente: str):
         self.patente = patente
+        self.asientos: list[Asiento] = [Asiento(i) for i in range(1, 51)] # genera lista con 50 asientos desocupados (list comprehension)
+    def obtenerAsientosLibres(self)->list[Asiento]:
+        return [asiento for asiento in self.asientos if not asiento.verificarOcupacion()] #genera una lista con los asientos libres  (list comprehension)
+    def verificarAsientoLibre(self, nroAsiento:int)->bool:
+        return not self.asientos[nroAsiento -1].verificarOcupacion()
+    def cambiarEstadoAsiento(self,nroAsiento:int):
+        self.asientos[nroAsiento - 1].cambiarEstado()
 
 
 class Ciudad:
@@ -17,36 +32,66 @@ class Ciudad:
         self.provincia = provincia 
 
 class Itinerario:
-    def __init__(self):
-        self.paradas = []
-
-
-class Servicio:
-    def __init__(self, unidad: Unidad, calidad: str, precio: int, itinerario: Itinerario):
-        self.unidad = unidad
-        self.calidad = calidad
-        self.precio = precio
-        self.itineriario = itinerario
-
-class Reserva:
-    def __init__(self, fechaHora: datetime):
-        self.fechaHora = fechaHora
+    def __init__(self, paradas:list[Ciudad]= []):
+        self.paradas = paradas
 
 class Pasajero:
     def __init__(self, nombre: str, email: str, dni: int):
         self.nombre = nombre
         self.email = email
         self.dni = dni
+   
+class Reserva:
+    def __init__(self, fechaHora: datetime, asiento: Asiento, pasajero:Pasajero):
+        self.fechaHora = fechaHora
+        self.asiento = asiento
+        self.pasajero = pasajero
+    #consultas
+    def obtenerAsientoNumero(self):
+        return self.asiento.obtenerNumero()
+    def obtenerFecha(self):
+        return self.fechaHora
 
-class Asiento:
-    def __init__(self, numero: int, ocupado: bool):
-        self.numero = numero
-        self.ocupado = ocupado
 
 class Venta: 
-    def __init__(self, fechaHora: datetime):
+    def __init__(self, fechaHora: datetime, asiento: Asiento):
         self.fechaHora = fechaHora
+        self.asiento = asiento
 
+
+class Servicio:
+    def __init__(self, unidad: Unidad, calidad: str, precio: float, itinerario: Itinerario):
+        self.unidad = unidad
+        self.calidad = calidad
+        self.precio = precio
+        self.itinerario = itinerario
+        self.ventas: list[Venta] = []
+        self.reservas: list[Reserva] = []
+#Asignaciones
+    def asignarUnidad(self, unidad:Unidad):
+        self.unidad= unidad
+    def asignarItinerario(self, itinerario: Itinerario):
+        self.itinerario = itinerario
+#Inserciones
+    def agregarReserva(self, reserva: Reserva) -> bool:
+        if not self.unidad.verificarAsientoLibre(reserva.obtenerAsientoNumero()):
+            raise ValueError("El asiento ya está ocupado.")
+        if reserva in self.reservas:
+            raise ValueError("La reserva ya existe.")
+        self.reservas.append(reserva)
+        self.unidad.cambiarEstadoAsiento(reserva.obtenerAsientoNumero())
+        return True
+#liberacion de reservas (se llama 30 min antes del viaje):
+    def liberarAsientosReservados(self):
+        for reserva in self.reservas:
+            i = reserva.obtenerAsientoNumero()
+            self.unidad.cambiarEstadoAsiento(i)
+        self.reservas.clear()
+#Consultas
+    def obtenerAsientosLibres(self):
+        return self.unidad.obtenerAsientosLibres()
+    
+############################################### MEDIOS DE PAGO  ###############################
 # interfaz 
 class MedioPago(ABC):
     
@@ -80,4 +125,8 @@ class Uala(MedioPago):
     def validarPago(self):
         return True
     
+################################################ CLASE SISTEMA ################################
 
+class ArgenTUR:
+    def __init__(self):
+        self.sistemaActivo = True
